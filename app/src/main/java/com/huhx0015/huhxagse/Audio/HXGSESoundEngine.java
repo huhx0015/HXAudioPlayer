40657bcb05c979a5b8123cef90b43aa9bb2419e3
Michael Y. Huh <mhuh@yoonhuh.com>
2015-04-01 18:06:34 -0700

    4/1/2015 (6:06 PM PST): * HXGSESoundEngine: Updated code comments.

--- app/src/main/java/com/huhx0015/huhxagse/Audio/HXGSESoundEngine.java
+++ app/src/main/java/com/huhx0015/huhxagse/Audio/HXGSESoundEngine.java
@@ -218,7 +218,8 @@ public class HXGSESoundEngine {
     // pauseSounds(): Pauses all sound effects playing in the background.
     public void pauseSounds() {
 
-        // Checks to see if hxgse_soundpool has been initiated first before pausing sound effect playback.
+        // Checks to see if the hxgse_soundpool object has been initiated first before pausing sound
+        // effect playback.
         if (hxgse_soundpool != null) {
             hxgse_soundpool.autoPause(); // Pauses all sound effect playback.
             Log.d(TAG, "SOUND: All sound playback has been paused.");
