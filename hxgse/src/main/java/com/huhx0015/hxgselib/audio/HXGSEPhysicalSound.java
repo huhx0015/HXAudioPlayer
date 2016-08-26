3a01613fe378b87136f7587d46d3cc53e53d6e73
Michael Y. Huh <mhuh@yoonhuh.com>
2016-08-26 01:06:42 -0700

    Updated SDK to 24, made changes to disablePhysSounds() for SDK 24 changes.

--- hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
+++ hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
@@ -2,6 +2,7 @@ package com.huhx0015.hxgselib.audio;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.os.Build;
 
 /** -----------------------------------------------------------------------------------------------
  *  [HXGSEPhysicalSound] CLASS
@@ -16,7 +17,19 @@ public class HXGSEPhysicalSound {
 
     // disablePhysicalSounds(): Enables or disables the devices physical button's sound effects.
     public static void disablePhysSounds(boolean mode, Context context) {
+
         AudioManager mgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (mode) {
+                mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
+            } else {
+                mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
+            }
+
+        } else {
+            mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode);
+        }
     }
 }
