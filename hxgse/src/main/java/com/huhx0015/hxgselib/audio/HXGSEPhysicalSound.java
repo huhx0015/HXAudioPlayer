61730724c4112becb6294be5ab66aff55211252a
huhx0015 <huhx0015@gmail.com>
2016-12-28 23:59:14 -0800

    HXGSEPhysicalSound: Added check for DO NOT DISTURB mode in disablePhysicalSounds() to address crash issue.

--- hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
+++ hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
@@ -3,6 +3,7 @@ package com.huhx0015.hxgselib.audio;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Build;
+import android.provider.Settings;
 
 /** -----------------------------------------------------------------------------------------------
  *  [HXGSEPhysicalSound] CLASS
@@ -20,16 +21,25 @@ public class HXGSEPhysicalSound {
 
         AudioManager mgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
+        // ANDROID 6.0+ (MARSHMALLOW):
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
-            if (mode) {
-                mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
-            } else {
-                mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
+            // Checks to see if 'Do Not Disturb' mode is on. Fixes a crash issue seen on Android
+            // 6.0+ devices.
+            try {
+                if (Settings.Global.getInt(context.getContentResolver(), "zen_mode") != 0) {
+                    if (mode) {
+                        mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
+                    } else {
+                        mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
+                    }
+                }
             }
 
-        } else {
-            mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode);
+            catch (Settings.SettingNotFoundException e) { e.printStackTrace(); }
         }
+
+        // ANDROID 2.3 - ANDROID 5.1:
+        else { mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode); }
     }
 }
