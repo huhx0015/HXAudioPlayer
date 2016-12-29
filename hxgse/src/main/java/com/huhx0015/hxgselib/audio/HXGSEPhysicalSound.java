0019aea32cd0696027b931ab464cd40fc10a2547
huhx0015 <huhx0015@gmail.com>
2016-12-29 10:20:56 -0800

    HXGSEPhysicalSound: Disabled disablePhysSounds() for Android 7.0 to address permissions crash issue.

--- hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
+++ hxgse/src/main/java/com/huhx0015/hxgselib/audio/HXGSEPhysicalSound.java
@@ -3,7 +3,6 @@ package com.huhx0015.hxgselib.audio;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Build;
-import android.provider.Settings;
 
 /** -----------------------------------------------------------------------------------------------
  *  [HXGSEPhysicalSound] CLASS
@@ -17,29 +16,29 @@ public class HXGSEPhysicalSound {
     /** CLASS FUNCTIONALITY ____________________________________________________________________ **/
 
     // disablePhysicalSounds(): Enables or disables the devices physical button's sound effects.
+    // Note: Android 7.0 and above requires "android.permission.ACCESS_NOTIFICATION_POLICY".
     public static void disablePhysSounds(boolean mode, Context context) {
 
-        AudioManager mgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        // ANDROID 2.3 - ANDROID 6.0:
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
 
-        // ANDROID 6.0+ (MARSHMALLOW):
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            AudioManager mgr = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
-            // Checks to see if 'Do Not Disturb' mode is on. Fixes a crash issue seen on Android
-            // 6.0+ devices.
-            try {
-                if (Settings.Global.getInt(context.getContentResolver(), "zen_mode") != 0) {
-                    if (mode) {
-                        mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
-                    } else {
-                        mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
-                    }
+            // ANDROID 6.0+:
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                if (mode) {
+                    mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
+                } else {
+                    mgr.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
                 }
+
             }
 
-            catch (Settings.SettingNotFoundException e) { e.printStackTrace(); }
+            // ANDROID 2.3 - ANDROID 5.1:
+            else {
+                mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode);
+            }
         }
-
-        // ANDROID 2.3 - ANDROID 5.1:
-        else { mgr.setStreamMute(AudioManager.STREAM_SYSTEM, mode); }
     }
 }
