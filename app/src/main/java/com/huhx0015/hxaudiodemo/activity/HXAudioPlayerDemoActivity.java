0500cdbf175ee532f009dd14b21969c8f00737f1
huhx0015 <mhuh@yoonhuh.com>
2026-04-25 09:08:01 -0700

    Updated project and Gradle configurations to compile on latest versions of Android Studio with AGP 9.0+
    Updated project and Gradle configurations to compile on latest versions of Android Studio with AGP 9.0+

--- app/src/main/java/com/huhx0015/hxaudiodemo/activity/HXAudioPlayerDemoActivity.java
+++ app/src/main/java/com/huhx0015/hxaudiodemo/activity/HXAudioPlayerDemoActivity.java
@@ -2,7 +2,7 @@ package com.huhx0015.hxaudiodemo.activity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
